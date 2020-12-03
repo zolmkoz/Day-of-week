dabebb6618f3f48c8f4cfba4d2ad54bd65451290
ZOLMKOZ <ZOLMKOZ@ZOLMKOZ-PC>
2020-12-03 08:12:40 +0700

    Add project files.

---
+++ Day of week/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization; //vung thoi gian
+
+namespace Day_of_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataAsText = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dataAsText, "d-M-yyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
